66037a885ebf1be7875abf64a6edf7f9619e07e7
Daffa Daraz <daffadaraz7@gmail.com>
2019-10-29 21:30:01 +0700

    Update Uang.java

--- Java/Uang/src/uang/Uang.java
+++ Java/Uang/src/uang/Uang.java
@@ -15,7 +15,6 @@ public class Uang {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
