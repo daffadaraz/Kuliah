6d2413822efd4ab7f164bbca1358415c3a0d8ca4
Daffa Daraz <daffadaraz7@gmail.com>
2019-11-08 23:36:40 +0700

    Update uangArray.java

--- Java/Kuliah/array/uangArray.java
+++ Java/Kuliah/array/uangArray.java
@@ -1,16 +1,37 @@
+import java.lang.reflect.Constructor;
 import java.util.Scanner;
+
+class minggu{
+    // 5 = Senin Selasa Rabu Kamis Jum'at
+    int uang[] = new int[5];
+
+    public minggu() {
+    }
+
+    public minggu(int uang[]) {
+        for(int i=0; i<uang.length ; i++){
+            this.uang[i] = uang[i];
+        }
+    }
+    
+}
 
 public class uangArray {
     public static Scanner scan = new Scanner (System.in);
-    public static String minggu[] = {"M1","M2","M3","M4"};
+    public static int mingguC = 1;
     public static String hari[] = {"Senin","Selasa","Rabu","Kamis","Jum'at"};
 
-    public static int[][] InputUang(){
-        int uang[][] = new int[minggu.length][hari.length];
+    public static minggu[] InputUang(){
+        minggu m1 = new minggu();
+        minggu m2 = new minggu();
+        minggu m3 = new minggu();
+        minggu m4 = new minggu();
 
-        for(int s = 0; s < minggu.length; s++){
+        minggu[] sebulan = {m1,m2,m3,m4};
+
+        for(int s = 0; s < mingguC; s++){
             System.out.println();
-            System.out.println("Minggu ke - "+minggu[s]);
+            System.out.println("Minggu ke - "+(s+1));
             for(int m = 0; m < hari.length ; m++){
                 System.out.println();
                 System.out.println(hari[m]);
@@ -23,13 +44,13 @@ public class uangArray {
                 System.out.print("Uang Transport = ");
                 uj = scan.nextInt();
                 
-                uang[s][m] = uj + ut;
+                sebulan[s].uang[m] = uj + ut;
             }
         }
-        return uang;
+        return sebulan;
     }
 
-    public static void PrintUang(int[][] uang,int uangMasuk,String nama){
+    public static void PrintUang(minggu[] sebulan,int uangMasuk,String nama){
         double constraint = uangMasuk*0.25;
 
         System.out.println();
@@ -43,19 +64,27 @@ public class uangArray {
         System.out.println();
         System.out.println("-----------------------------------------------------------------------------------------------------------");
     
-        for(int s = 0; s < minggu.length; s++){
+        for(int s = 0; s < mingguC; s++){
             int total = 0;
             String status = "Boros";
 
             for(int i = 0; i < hari.length; i++){
-                total += uang[s][i];
+                total += sebulan[s].uang[i];
             }
 
             if(total < constraint){
                 status = "Hemat";
             }
 
-            System.out.format("%-10s%-15s%-15s%-15s%-15s%-15s%-15s%-15s",minggu[s],uang[s][0],uang[s][1],uang[s][2],uang[s][3],uang[s][4],total,status);
+            System.out.format("%-10s%-15s%-15s%-15s%-15s%-15s%-15s%-15s",
+                ("M"+(s+1)),
+                sebulan[s].uang[0],
+                sebulan[s].uang[1],
+                sebulan[s].uang[2],
+                sebulan[s].uang[3],
+                sebulan[s].uang[4],
+                total,status);
+
             System.out.println();
         }
 
@@ -63,15 +92,30 @@ public class uangArray {
     }
 
     public static void main(String[] args) {
+        String ulang = "Y";
+        while(ulang.contains("Y") || ulang.contains("y")){
+            System.out.println("\n---------------------------------");
+            System.out.print("Jumlah Minggu (1..4) : ");
+            mingguC = scan.nextInt(); scan.nextLine();
+            if(mingguC < 1 || mingguC > 4){
+                System.out.println("\nJumlah Minggu hanya 1 sampai 4");
+                return;
+            }
+            
+            System.out.print("Nama Pelapor : ");
+            String nama = scan.nextLine();
 
-        System.out.print("Nama Pelapor : ");
-        String nama = scan.nextLine();
+            System.out.print("Uang Masuk   : ");
+            int uangMasuk = scan.nextInt();
 
-        System.out.print("Uang Masuk   : ");
-        int uangMasuk = scan.nextInt();
+            PrintUang(InputUang(),uangMasuk,nama);
+            
+            scan.nextLine();
+            System.out.println("\n");
+            System.out.print("Ulang program ? (y/n) ");
+            ulang = scan.nextLine();
 
-        PrintUang(InputUang(),uangMasuk,nama);
-        
+        }
         scan.close();
     }
 }
